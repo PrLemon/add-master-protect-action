40a6e9dd80bc5db3005ceffca9a1aa5fe2047d43
rohitnb <rohitnb91@gmail.com>
2020-07-29 23:49:59 +0530

    what happens now

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(github.context.payload.head_commit.author.email)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
