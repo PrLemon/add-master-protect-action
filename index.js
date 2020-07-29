82369adc5e2453cda02808ca09eee9914302e5ed
rohitnb <rohitnb91@gmail.com>
2020-07-30 00:03:31 +0530

    structure

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log("Does this even work? 30 Jul"+github.context.payload.head_commit.author.email)
+  console.log("Does this even work? 30 Jul"+github.context.payload)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
