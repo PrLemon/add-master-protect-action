46895af46742e0e556fab0d7116dec217f27cf2b
rohitnb <rohitnb91@gmail.com>
2020-07-30 12:48:05 +0530

    fix

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const commiter = github.context.payload.commits[0].committer.username
   if (github.context.ref.includes("master")){
-    const newIssue = await octokit.issues.create({
+    const newIssue = octokit.issues.create({
       ...context.repo,
       title: 'New commit on master detected!',
       body: 'The commit was made by '+commiter
