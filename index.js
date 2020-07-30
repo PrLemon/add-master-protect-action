0b36ff738d046c1743caf81a692f46463e6767bc
rohitnb <rohitnb91@gmail.com>
2020-07-30 14:28:14 +0530

    make async

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  const token = core.getInput('mytoken'); //get the user token from as an input from the Workflow file
-  const octokit = github.getOctokit(token); //instantiate the octokit using the token
-  //const octokit = new github.GitHub(token);
-  //https://octokit.github.io/rest.js/v18
-  //Use the functions described 
+async function run() {
 
-  //Logic for creating an issue when a commit on master is detected!
-  if (github.context.ref.includes("master")){
-    const commiter = github.context.payload.commits[0].committer.username;
-    const newIssue = await octokit.issues.create({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      title: 'New commit on master detected!',
-      body: 'The commit was made by '+commiter
-    });
-    console.log("new issue"+JSON.stringify(newIssue.data)+" and type is "+typeof(newIssue)); 
+  try {
+    const token = core.getInput('mytoken'); //get the user token from as an input from the Workflow file
+    const octokit = github.getOctokit(token); //instantiate the octokit using the token
+    //const octokit = new github.GitHub(token);
+    //https://octokit.github.io/rest.js/v18
+    //Use the functions described 
+
+    //Logic for creating an issue when a commit on master is detected!
+    if (github.context.ref.includes("master")){
+      const commiter = github.context.payload.commits[0].committer.username;
+      const newIssue = await octokit.issues.create({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        title: 'New commit on master detected!',
+        body: 'The commit was made by '+commiter
+      });
+      console.log("new issue"+JSON.stringify(newIssue.data)+" and type is "+typeof(newIssue)); 
+    }
+    //end
+  } catch (error) {
+    core.setFailed(error.message);
   }
-  //end
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+run();
