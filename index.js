a2f8cc24da05af1d2e060d5d79a458966e8e62ef
rohitnb <rohitnb91@gmail.com>
2020-07-30 12:46:25 +0530

    Test branch

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  // const nameToGreet = core.getInput('who-to-greet');
+  // console.log(`Hello ${nameToGreet}!`);
+  // const time = (new Date()).toTimeString();
+  // core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const str_context = JSON.stringify(github.context);
-  console.log("Payload is of Type "+typeof(github.context.payload));
-  console.log("The commiter is "+github.context.payload.commits[0].author.username+" and its of type "+typeof(github.context.payload.commits[0].author.username));
-  console.log("Repo Var is of Type "+typeof(github.context.repo));
-  console.log("=========Context Var ENDS==========")
+  const commiter = github.context.payload.commits[0].committer.username
+  if (github.context.ref.includes("master")){
+    const newIssue = await octokit.issues.create({
+      ...context.repo,
+      title: 'New commit on master detected!',
+      body: 'The commit was made by '+commiter
+    }); 
+  }
+   
   //const payload = JSON.stringify(github.context.payload, undefined, 2)
   //console.log(`The event payload: ${payload}`);
 } catch (error) {
