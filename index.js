70f51cae3513006e55a57a89a86eba9776eeed93
rohitnb <rohitnb91@gmail.com>
2020-07-30 14:18:21 +0530

    await

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const github = require('@actions/github');
 
 try {
   const token = core.getInput('mytoken'); //get the user token from as an input from the Workflow file
-  //const octokit = github.getOctokit(token); //instantiate the octokit using the token
-  const octokit = new github.GitHub(token);
+  const octokit = github.getOctokit(token); //instantiate the octokit using the token
+  //const octokit = new github.GitHub(token);
   //https://octokit.github.io/rest.js/v18
   //Use the functions described 
 
   //Logic for creating an issue when a commit on master is detected!
   if (github.context.ref.includes("master")){
     const commiter = github.context.payload.commits[0].committer.username;
-    const newIssue = octokit.issues.create({
+    const newIssue = await octokit.issues.create({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       title: 'New commit on master detected!',
