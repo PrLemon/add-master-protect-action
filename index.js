558f48a486fb5f7514ab9d336b837c7baf08c2fe
rohitnb <rohitnb@github.com>
2020-09-05 16:27:55 +0530

    Loggin-3

--- index.js
+++ index.js
@@ -7,12 +7,11 @@ async function run() {
   try {
     const token = core.getInput('mytoken'); //get the user token from as an input from the Workflow file
     const required_approvers = core.getInput('required_approvers');
-    console.log("Here?");
     const octokit = new Octokit({ auth: token });
     //const octokit = github.getOctokit(token); //instantiate the octokit using the token
     //https://octokit.github.io/rest.js/v18
     //Use the functions described 
-
+    console.log(token)
     const what = await octokit.request('PUT /repos/{owner}/{repo}/branches/{branch}/protection', {
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
